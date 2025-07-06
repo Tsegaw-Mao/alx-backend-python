55535e3cb6ea4770af8eace4d98e0387f4f2224b
Tsegaw-Mao <tsegawjohnj@gmail.com>
2025-07-06 12:49:46 +0300

    python

--- python-generators-0x00/1-batch_processing.py
+++ python-generators-0x00/1-batch_processing.py
@@ -23,14 +23,17 @@ def stream_users_in_batches(batch_size):
 
 def batch_processing(batch_size):
     """
-    Processes batches of users, filtering those over age 25.
-    Prints them.
+    Processes batches of users, filters those over age 25,
+    and returns a list of all such users.
     """
+    filtered_users = []
     for batch in stream_users_in_batches(batch_size):
         filtered = [user for user in batch if float(user[3]) > 25]  # age is at index 3
-        for user in filtered:
-            print(user)
+        filtered_users.extend(filtered)
+    return filtered_users
 
 
 if __name__ == "__main__":
-    batch_processing(10)
+    result = batch_processing(10)
+    for user in result:
+        print(user)
