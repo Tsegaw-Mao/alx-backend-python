c649ed6723fef05c6685e5f95977a1449f9b8519
Tsegaw-Mao <tsegawjohnj@gmail.com>
2025-07-21 00:39:48 +0300

    fix

--- 0x03-Unittests_and_integration_tests/fixtures.py
+++ 0x03-Unittests_and_integration_tests/fixtures.py
@@ -948,3 +948,26 @@ TEST_PAYLOAD = [
     ['dagger', 'kratu', 'traceur-compiler', 'firmata.py'],
   )
 ]
+
+# fixtures.py
+
+repos_payload = [
+    {
+        "id": 1,
+        "name": "repo1",
+        "license": {"key": "apache-2.0"}
+    },
+    {
+        "id": 2,
+        "name": "repo2",
+        "license": {"key": "mit"}
+    },
+    {
+        "id": 3,
+        "name": "repo3",
+        "license": {"key": "apache-2.0"}
+    }
+]
+
+apache2_repos = ['repo1', 'repo3']
+
