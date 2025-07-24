ae7ce6cac201a89789eef263f22d67d1bf2630ea
Tsegaw-Mao <tsegawjohnj@gmail.com>
2025-07-24 16:10:11 +0300

    Restrict Chat Access by time

--- Django-Middleware-0x03/chats/middleware.py
+++ Django-Middleware-0x03/chats/middleware.py
@@ -1,27 +1,39 @@
 # messaging_app/middleware.py
 
 import logging
-from datetime import datetime
+from datetime import datetime, time
 from django.http import HttpResponseForbidden
+
 
 class RequestLoggingMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
         self.logger = logging.getLogger("request_logger")
         handler = logging.FileHandler("requests.log")  # log file
-        formatter = logging.Formatter('%(message)s')
+        formatter = logging.Formatter("%(message)s")
         handler.setFormatter(formatter)
         if not self.logger.handlers:
             self.logger.addHandler(handler)
         self.logger.setLevel(logging.INFO)
 
     def __call__(self, request):
-         # Define allowed access window
+        user = request.user if request.user.is_authenticated else "Anonymous"
+        log_message = f"{datetime.now()} - User: {user} - Path: {request.path}"
+        self.logger.info(log_message)
+        return self.get_response(request)
+
+
+class RestrictAccessByTimeMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        # Define allowed access window
         now = datetime.now().time()
         start_time = time(18, 0)  # 6:00 PM
-        end_time = time(21, 0)    # 9:00 PM
-        
-         # Only block API/chat paths; adjust if needed
+        end_time = time(21, 0)  # 9:00 PM
+
+        # Only block API/chat paths; adjust if needed
         if request.path.startswith("/api/"):
             if not (start_time <= now <= end_time):
                 return HttpResponseForbidden(
@@ -29,8 +41,4 @@ class RequestLoggingMiddleware:
                 )
 
         response = self.get_response(request)
-        
-        user = request.user if request.user.is_authenticated else "Anonymous"
-        log_message = f"{datetime.now()} - User: {user} - Path: {request.path}"
-        self.logger.info(log_message)
         return response
