3c22776559e97c135dd453ecce62f3352d3f8793
Tsegaw-Mao <tsegawjohnj@gmail.com>
2025-07-24 12:10:39 +0300

    Pagination and Filtering

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -138,7 +138,7 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
     'DEFAULT_PAGINATION_CLASS': 'chats.pagination.MessagePagination',  # ✅ Custom pagination
-    'PAGE_SIZE': 20,  # Optional fallback
+    'PageNumberPagination': 20,  # Optional fallback
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
 }
 
