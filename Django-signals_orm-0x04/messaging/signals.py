d321fb63f6cb13178d45d2a353b0a4dfb844a066
Tsegaw-Mao <tsegawjohnj@gmail.com>
2025-08-03 11:20:36 +0300

    post_save

--- Django-signals_orm-0x04/messaging/signals.py
+++ Django-signals_orm-0x04/messaging/signals.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.models import User
-from django.db.models.signals import pre_save, post_delete
+from django.db.models.signals import pre_save, post_delete, post_save
 from django.dispatch import receiver
 from .models import Message, MessageHistory, Notification
 
@@ -36,3 +36,11 @@ def log_message_edit(sender, instance, **kwargs):
         )
         # Mark the message as edited
         instance.edited = True
+        
+@receiver(post_save, sender=Message)
+def create_notification_on_new_message(sender, instance, created, **kwargs):
+    if created:
+        Notification.objects.create(
+            user=instance.receiver,
+            message=instance
+        )
